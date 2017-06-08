12080c082c5b98d3716577e375ddb8e52ddd10be
Nazar Korchevsky <naaa.nazar@gmail.com>
2017-06-08 12:05:49 +0300

    fix code

--- js/slyder-js.js
+++ js/slyder-js.js
@@ -12,10 +12,6 @@ $( document ).ready(function() {
         counterImg++;
     });
 
-    console.log(counterImg);
-    console.log(imagesSrc);
-
-
     $(document).on('click', '.slider_right_button', function(){
 
         // $('.slider_img').addClass('animated slideInLeft');
@@ -37,31 +33,11 @@ $( document ).ready(function() {
             }
         }
 
-
-        $('.slider_img').each(function(n,elem) {
-            var html = '' +
-                '<div class="slider_img_block">' +
-                '<img class="slider_img animated slideInRight"  src="' + imagesSrc[n] + '" alt="">' +
-                '</div>';
-
-       /*     console.log($("." + elem + ":eq( " + i + " )").closest('.slider_img_block'));*/
-
-            console.log('n---' + n);
-            $(elem).closest('.slider_img_block').after(html);
-
-        }),500;
-
-        console.log(imagesSrc);
-
-        setTimeout(function(){$('.slider_img').removeClass('animated')}, 2000);
-        setTimeout(function(){$('.slider_img').removeClass('slideInLeft')}, 2000);
-        setTimeout(function(){$('.slider_img_prev').removeClass('slider_img')});
-        setTimeout(function(){$('.slider_img_block_prev').remove()},1000);
-
-    })
+        addNewSlide('slideInRight', imagesSrc);
+        removElementsAfterClick();
+    });
 
     $(document).on('click', '.slider_left_button', function(){
-
 
         $('.slider_img').closest('.slider_img_block').addClass('slider_img_block_prev');
         $('.slider_img').addClass('slider_img_prev');
@@ -80,27 +56,30 @@ $( document ).ready(function() {
             }
         }
 
-        $('.slider_img').each(function(n,elem) {
-            var html = '' +
-                '<div class="slider_img_block">' +
-                '<img class="slider_img animated slideInLeft"  src="' + imagesSrc[n] + '" alt="">' +
-                '</div>';
-
-            /*     console.log($("." + elem + ":eq( " + i + " )").closest('.slider_img_block'));*/
-
-            console.log('n---' + n);
-            $(elem).closest('.slider_img_block').after(html);
-
-        }),500;
-
-        console.log(imagesSrc);
-
-        setTimeout(function(){$('.slider_img').removeClass('animated')}, 2000);
-        setTimeout(function(){$('.slider_img').removeClass('slideInLeft')}, 2000);
-        setTimeout(function(){$('.slider_img_prev').removeClass('slider_img')});
-        setTimeout(function(){$('.slider_img_block_prev').remove()},1000);
-
-    })
+        addNewSlide('slideInLeft', imagesSrc);
+        removElementsAfterClick();
+    });
 
 
 });
+
+var removElementsAfterClick = function(){
+    setTimeout(function(){$('.slider_img').removeClass('animated')}, 2000);
+    setTimeout(function(){$('.slider_img_prev').removeClass('slider_img')});
+    setTimeout(function(){$('.slider_img_block_prev').remove()},1000);
+}
+
+var addNewSlide = function(animeteClass, imagesSrc){
+    $('.slider_img').each(function(n,elem) {
+        var html = '' +
+            '<div class="slider_img_block">' +
+            '<img class="slider_img animated ' + animeteClass + '"  src="' + imagesSrc[n] + '" alt="">' +
+            '</div>';
+
+        /*     console.log($("." + elem + ":eq( " + i + " )").closest('.slider_img_block'));*/
+
+        console.log('n---' + n);
+        $(elem).closest('.slider_img_block').after(html);
+
+    }),500;
+}
